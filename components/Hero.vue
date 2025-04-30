b8834dae6f09d6a057033b82597b0080148982a3
Alan Mark <alanmark2001@gmail.com>
2025-04-30 18:04:51 +0530

    add: hero page

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,31 +1,56 @@
+<script setup>
+import { ArrowRight } from 'lucide-vue-next';
+</script>
+
+
 <template>
-    <div class="pt-20">
-        <div class="">
-            The #1 AI Headshot Generator for Professional Headshots
-        </div>
-        <div>
-            Professional business headshots, without a physical photo shoot
-        </div>
-        <div>
-            Get professional business headshots in minutes with our AI headshot generator. Upload photos, pick your
-            styles & receive 100+ headshots.
-        </div>
-        <div>
-            Create your headshots now
-        </div>
-        <div>
-            Team Packages
-        </div>
-        <div>
-            Rated 4.8 out of 5 with 2,302 reviews on
+    <div class="pt-34 flex items-start md:items-center justify-center flex-col w-full gap-4 p-4 bg-gray-50">
+        <div
+            class="flex items-start md:items-center justify-center flex-col w-full gap-4 p-4 bg-gray-50 max-w-[700px] text-center">
 
+            <div class="text-gray-400 text-sm font-semibold">
+                The #1 AI Headshot Generator for Professional Headshots
+            </div>
+            <div class="text-[#2A175D] font-bold text-4xl mt-3">
+                Professional business headshots, without a physical photo shoot
+            </div>
+            <div class="text-lg">
+                Get professional business headshots in minutes with our AI headshot generator. Upload photos, pick your
+                styles & receive 100+ headshots.
+            </div>
+            <div class="flex gap-3 items-center">
+                <div class="bg-[#FF6600] text-white font-bold text-md flex items-center gap-2 p-3 px-5 rounded-md">
+                    Create your headshots now
+                    <ArrowRight />
+                </div>
+                <span class="text-[#a5a4a7] font-semibold text-xs">OR</span>
+                <div class="text-[#2A175D] font-semibold text-md border-[0.2px] rounded-md p-3 px-5 border-[#e7e7e7]">
+                    Team Packages
+                </div>
+            </div>
 
-            18M
-            headshots created for
-            102,207+
-            happy customers
-
-
+            <div>
+                <div class="">
+                    <div class="flex gap-4 items-center">
+                        <img src="/trustpilot.svg" alt="logo" class="w-30 inline" />
+                        <div class="text-gray-700"> Rated <span class="font-semibold text-black">4.8</span> out of 5
+                            with <span class="font-semibold text-black">2,302</span> reviews on</div>
+                        <img src="/trust.png" alt="logo" class="w-24 inline" />
+                    </div>
+                    <div class="text-[0.9rem] text-gray-500 mt-3">
+                        <span class="font-semibold">18M</span>
+                        headshots created for
+                        <span class="font-semibold">102,207+</span>
+                        happy customers
+                    </div>
+                </div>
+            </div>
+            <div class="marquee-container">
+                <div class="marquee flex gap-3 scale-[4]">
+                    <img src="/logo-cloud-horizontal.e5e3772.png" alt="logo" class="marquee-img" />
+                    <img src="/logo-cloud-horizontal.e5e3772.png" alt="logo" class="marquee-img" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,16 @@
+<script setup>
+import { Menu } from 'lucide-vue-next';
+</script>
+
 <template>
-    <nav class="p-3 px-8 flex items-center gap-10 justify-between backdrop-blur-lg border-b border-[0.2px] border-[#eeeeee] fixed w-full">
+    <nav
+        class="p-3 px-4 md:px-8 flex items-center gap-10 justify-between backdrop-blur-lg border-b border-[0.2px] border-[#eeeeee] fixed w-full">
         <div class="flex gap-8 items-center">
             <div class="flex gap-2 items-center">
                 <img src="/loho.svg" alt="logo" class="w-40" /> <a href="#"
                     class="bg-[#64CDCF] font-bold text-white flex items-center text-[0.6rem] h-4 rounded-xl px-2 uppercase">Hiring</a>
             </div>
-            <div class="flex gap-5 text-lg text-[#1C145D] font-semibold">
+            <div class="md:flex gap-5 text-lg text-[#1C145D] font-semibold hidden ">
                 <div>
                     For teams
                 </div>
@@ -19,8 +24,8 @@
             <button class="bg-[#1C145D] text-white font-medium p-4 py-1 rounded-md">
                 Create
             </button>
-            <button>
-                Menu
+            <button class="border-[0.2px] border-[#e7e7e7] p-3 py-1 rounded-md">
+                <Menu />
             </button>
 
         </div>
