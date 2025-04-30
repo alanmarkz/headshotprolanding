5c8820e8f8379dbe5cb610772324b9fe0dc32c10
Alan Mark <alanmark2001@gmail.com>
2025-04-30 16:59:00 +0530

    headshotpro

---
+++ components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pt-20">
+        <div class="">
+            The #1 AI Headshot Generator for Professional Headshots
+        </div>
+        <div>
+            Professional business headshots, without a physical photo shoot
+        </div>
+        <div>
+            Get professional business headshots in minutes with our AI headshot generator. Upload photos, pick your
+            styles & receive 100+ headshots.
+        </div>
+        <div>
+            Create your headshots now
+        </div>
+        <div>
+            Team Packages
+        </div>
+        <div>
+            Rated 4.8 out of 5 with 2,302 reviews on
+
+
+            18M
+            headshots created for
+            102,207+
+            happy customers
+
+
+        </div>
+    </div>
+</template>
---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="p-3 px-8 flex items-center gap-10 justify-between backdrop-blur-lg border-b border-[0.2px] border-[#eeeeee] fixed w-full">
+        <div class="flex gap-8 items-center">
+            <div class="flex gap-2 items-center">
+                <img src="/loho.svg" alt="logo" class="w-40" /> <a href="#"
+                    class="bg-[#64CDCF] font-bold text-white flex items-center text-[0.6rem] h-4 rounded-xl px-2 uppercase">Hiring</a>
+            </div>
+            <div class="flex gap-5 text-lg text-[#1C145D] font-semibold">
+                <div>
+                    For teams
+                </div>
+                <div>
+                    Examples
+                </div>
+                <div>Pricing</div>
+            </div>
+        </div>
+        <div class="flex gap-5">
+            <button class="bg-[#1C145D] text-white font-medium p-4 py-1 rounded-md">
+                Create
+            </button>
+            <button>
+                Menu
+            </button>
+
+        </div>
+    </nav>
+</template>
